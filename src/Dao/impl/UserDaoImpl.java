4f4d1c671f8779bfff3e0bbdccf4621ef6a2048d
Administrator <Administrator@SC-201909131415>
2020-03-02 16:44:09 +0800

    修改dao中操作

--- src/Dao/impl/UserDaoImpl.java
+++ src/Dao/impl/UserDaoImpl.java
@@ -8,15 +8,21 @@ import entity.User;
 
 public class UserDaoImpl implements userdao {
 	//实现dao中的接口操作类
+	List<User> list=new ArrayList<User>();
 	@Override
 	public void save(User user) {
 		// TODO Auto-generated method stub
 		//创建集合保存用户信息
-		List<User> list=new ArrayList<User>();
 		list.add(user);
 		for(User user2:list) {
 			System.out.println(user2.getName()+","+user2.getAge());
 		}
+	}
+
+	@Override
+	public void updata(User user) {
+		// TODO Auto-generated method stub
+		System.out.println("执行修改用户信息的操作");
 	}
 
 }
--- src/Dao/userdao.java
+++ src/Dao/userdao.java
@@ -3,4 +3,5 @@ import entity.User;
 public interface userdao {
 	//保存用户信息方法
  void save(User user);
+ void updata(User user);
 }
